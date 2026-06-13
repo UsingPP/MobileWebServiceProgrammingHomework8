db437ecfa06ae5ad64560b8b1f30553b703f5db2
Yeongwon Kim <wsx1386@gmail.com>
2026-06-14 01:40:16 +0900

    update

--- blog/views.py
+++ blog/views.py
@@ -19,14 +19,14 @@ def post_detail(request,pk):
 def post_new(request) :
     if request.method == "POST" :
         form = PostForm()
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author= request.user
-        post.published_date= timezone.now()
-        post.save()
-        return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author= request.user
+            post.published_date= timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
