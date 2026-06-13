a6c61c173dcbdb448a4a66c9cc538619ec57adf0
usingPP <wsx1386@gmail.com>
2026-06-13 15:16:35 +0000

    수정사항

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model) :
     published_data = models.DateTimeField(blank = True, null= True)
 
     def publish(self) :
-        self.publichsd_data = timezone.now()
+        self.published_data = timezone.now()
         self.save()
 
     def __str__(self) :
